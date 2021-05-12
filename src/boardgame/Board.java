c700091ddd6aa0964d6954bdc5b5ca80a2acd56b
Marco Picanço <mrcpicanco@gmail.com>
2021-05-12 19:12:34 -0300

    Colocar as peças no tabuleiro.

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -35,7 +35,12 @@ public class Board {
 	}
 	
 	
-	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position; //a posição da peça está acessível porque está na classe Board(Protected)
+		
+		
+	}
 	
 
 }
