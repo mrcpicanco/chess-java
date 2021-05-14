548d0b399742877b76a5f6b5a39560e5a1a60a0b
Marco Picanço <mrcpicanco@gmail.com>
2021-05-13 23:45:17 -0300

    Movimentos possíveis para a peça.

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected Position position;
 	private Board board;
@@ -16,6 +16,28 @@ public class Piece {
 	protected Board getBoard() { //Somente classes do mesmo pacote e subclasses é que poderão acessar o tabuleiro de uma peça;
 		return board;
 	}
+	
+	
+	//Operçaão de movimentos possíveis que depende de um método abstrato
+	
+	public abstract boolean [][] possibleMoves();
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+		
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat.length; j++) {
+				if(mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
 
 	/* - O método set é retirado para que o tabuleiro não seja alterado.
 	public void setBoard(Board board) {
